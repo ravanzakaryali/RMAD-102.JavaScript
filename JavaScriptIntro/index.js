cd9789343260aec410fcf7f540778faf83b5b772
ravanzli <revanzekeryeli2002@gmail.com>
2023-04-11 11:40:39 +0400

    added js intro

---
+++ JavaScriptIntro/index.js
@@ -0,0 +1,103 @@
+"use strict"
+//JavaScript declare variable - var, const, let, nothing
+// const number2 = 20;
+// number2 = 50;
+let number = 10;
+console.log(number);
+//let - scop variable
+//var - scop variable deyil
+
+// console.log(number3);
+
+// var number3 = 10;
+
+//JavaScript types - boolean, number,string, object, undefined, null, bigint, Symbol
+// let flag = true;
+
+// let user = {}
+
+// let number = 1000000000000000n;
+
+
+// let name = 'Ka ';
+// let number = 10;
+
+// let number2 = null;
+
+// let age = 30;
+
+// if(age<=18){
+//     console.log("Usaqsan");
+// }else if(age<=35){
+//     console.log("Yetkin");
+// }else{
+//     console.log("Else")
+// }
+
+// || &&
+
+// if(age < 18 || age < 120){
+//     console.log("Yetkin");
+// }
+
+// let day = 7;
+
+
+// switch (day) {
+//     case 1:
+//         console.log("Pr");
+//         break;
+//     case 2:
+//         console.log("Çr a");
+//         break;
+//     case 3:
+//         console.log("Çr");
+//         break;
+//     case 6:
+//     case 7:
+//         console.log("Hefte sonlari");
+//         break;
+//     default:
+//         console.log("Basqa gunler");
+//         break;
+// }
+
+// for(let i = 1;i<100;i++){
+//     if(i%3!=0){
+//         console.log(i);
+//     }
+// }
+// let count = 0;
+// while(count < 10){
+//     console.log(count++);
+// }
+
+// do{
+//     console.log(co)
+//     count++;
+// }while(count < 10)
+
+
+// for (let i = 0; i < 10; i++) {
+//     if(i == 5){
+//         // break;
+//         continue;
+//     }    
+//     console.log(i);
+// }
+
+
+// for(let i = 0; i< 10;i++){
+//     for(let k = 0; k<10;k++){
+//         console.log("i - ",i);
+//         console.log("k - ",k);
+//     }
+// }
+
+
+
+// for(let i = 1;i<Infinity;i++){
+//     if(i%3!=0){
+//         console.log(i);
+//     }
+// }
